370529cd10de8749c65ebe6990434a0ea7660b16
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2023-07-12 22:45:27 +0530

    Added demo for single select

--- src/stories/TagPicker.stories.tsx
+++ src/stories/TagPicker.stories.tsx
@@ -12,26 +12,73 @@ const meta: Meta<typeof TagPicker> = {
 
 type Story = StoryObj<typeof TagPicker>;
 
-function TagPickerWithHooks() {
+function MultiSelectTagPicker() {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   return (
-    <TagPicker
-      renderBadge={tag => (
+    <div className='flex flex-col items-center gap-10'>
+      <TagPicker
+        renderBadge={tag => (
+          <Badge
+            key={tag.id}
+            onClick={() => setSelectedTags([...selectedTags, tag])}
+            className={cn('cursor-pointer', colorCircle({ color: tag.color }))}
+          >
+            {tag.name}
+          </Badge>
+        )}
+      />
+      <h2 className='text-2xl font-bold'>Selected Tags:</h2>
+      {selectedTags.map(tag => (
         <Badge
-          key={tag.id}
-          onClick={() => setSelectedTags([...selectedTags, tag])}
+          // onClick={() => setSelectedTag(null)}
           className={cn('cursor-pointer', colorCircle({ color: tag.color }))}
         >
           {tag.name}
         </Badge>
+      ))}
+    </div>
+  );
+}
+
+function SingleSelectTagPicker() {
+  const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
+
+  return (
+    <div className='flex flex-col items-center gap-10'>
+      <TagPicker
+        renderBadge={tag => (
+          <Badge
+            key={tag.id}
+            onClick={() => setSelectedTag(tag)}
+            className={cn('cursor-pointer', colorCircle({ color: tag.color }))}
+          >
+            {tag.name}
+          </Badge>
+        )}
+      />
+      <h2 className='text-2xl font-bold'>Selected Tag:</h2>
+      {selectedTag && (
+        <Badge
+          // onClick={() => setSelectedTag(null)}
+          className={cn(
+            'cursor-pointer',
+            colorCircle({ color: selectedTag.color })
+          )}
+        >
+          {selectedTag.name}
+        </Badge>
       )}
-    />
+    </div>
   );
 }
 
 export default meta;
 
-export const Primary: Story = {
-  render: () => <TagPickerWithHooks />,
+export const MultiSelect: Story = {
+  render: () => <MultiSelectTagPicker />,
+};
+
+export const SingleSelect: Story = {
+  render: () => <SingleSelectTagPicker />,
 };
